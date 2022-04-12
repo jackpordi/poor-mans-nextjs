4260db95084191342ec359cbaaeb140ecf1ddbba
Jack Pordi <jackel119@gmail.com>
2022-04-12 22:27:46 +0100

    init

---
+++ src/main.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import express, { Application, Request, Response } from 'express';
+import ReactDOMServer from "react-dom/server";
+import { HomePage } from './pages';
+
+const app: Application = express()
+
+const port = 3000;
+
+app.get('/', (_: Request, res: Response) => {
+    // @ts-ignore
+    const stream = ReactDOMServer.renderToPipeableStream(<HomePage />, {
+        onAllReady() {
+            res.setHeader('Content-Type', 'text/html');
+        }
+    });
+     stream.pipe(res, { end: false });
+})
+
+app.listen(port, function () {
+    console.log(`App is listening on port ${port}!`)
+})
---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from "react";
+
+export const HomePage: FC = () => (
+  <html>
+    <body>
+      <div
+        style={{
+          display: "flex",
+          flex: 1,
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        Hello world!
+      </div>
+    </body>
+  </html>
+);
